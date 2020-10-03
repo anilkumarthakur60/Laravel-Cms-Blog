f13a686cccd481d9ed638c7c561b87e8aa6bb9a3
anil kumar thakur <anilkumarthakur60@gmail.com>
2020-10-03 22:12:35 +0545

    initial

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -32,9 +32,8 @@ Create Post
             </div> 
             
             <div class="form-group "  >
-                <textarea  class="form-control my-editor" id="" name="content" cols="30" rows="5"> {{isset($post)? $post->content :''}}</textarea>
-                
-            <textarea id="my-editor" name="content" class="form-control"> {{isset($post)? $post->content :''}}</textarea>
+                    
+            <textarea id="content" name="content" class="form-control"> {{isset($post)? $post->content:''}}</textarea>
              </div> 
             @if (isset($post))
             <div class="form-group">
@@ -142,60 +141,20 @@ Create Post
     $('.tag_selector').select2();
 });
 </script>
-{{-- <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-<script>
-    var editor_config = {
-      path_absolute : "/",
-      selector: "textarea.my-editor",
-      plugins: [
-        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-        "searchreplace wordcount visualblocks visualchars code fullscreen",
-        "insertdatetime media nonbreaking save table contextmenu directionality",
-        "emoticons template paste textcolor colorpicker textpattern"
-      ],
-      toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
-      relative_urls: false,
-      file_browser_callback : function(field_name, url, type, win) {
-        var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-        var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
-  
-        var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
-        if (type == 'image') {
-          cmsURL = cmsURL + "&type=Images";
-        } else {
-          cmsURL = cmsURL + "&type=Files";
-        }
-  
-        tinyMCE.activeEditor.windowManager.open({
-          file : cmsURL,
-          title : 'Filemanager',
-          width : x * 0.8,
-          height : y * 0.8,
-          resizable : "yes",
-          close_previous : "no"
-        });
-      }
-    };
-  
-    tinymce.init(editor_config);
-</script>
- --}}
 
 
-<script src="//cdn.ckeditor.com/4.6.2/standard/ckeditor.js"></script>
-<script>
-  var options = {
-    filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
-    filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
-    filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
-    filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
-  };
-  
-CKEDITOR.replace('my-editor', options);
-</script>
-
-
-
+  <!-- CKEditor init -->
+  <script src="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.5.11/ckeditor.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.5.11/adapters/jquery.js"></script>
+  <script>
+    $('textarea[id=content]').ckeditor({
+      height: 100,
+      filebrowserImageBrowseUrl: route_prefix + '?type=Images',
+      filebrowserImageUploadUrl: route_prefix + '/upload?type=Images&_token={{csrf_token()}}',
+      filebrowserBrowseUrl: route_prefix + '?type=Files',
+      filebrowserUploadUrl: route_prefix + '/upload?type=Files&_token={{csrf_token()}}'
+    });
+  </script>
 @endsection
 
 
