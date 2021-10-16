3ba2ab1b02764e0ca0f8e798975684caf81165bf
anil kumar thakur <anilkumarthakur60@gmail.com>
2021-10-16 18:53:37 +0545

    commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,107 +12,141 @@ use App\Tag;
 use Facade\Ignition\Tabs\Tab;
 
 class PostController extends Controller
-{   
+{
     public function __construct()
     {
-        $this->middleware('verifyCategoryCount')->only(['create','store']);
-        
+        $this->middleware('verifyCategoryCount')->only(['create', 'store']);
     }
-  
-    public function index() 
-    { 
-        return view('posts.index')->with('posts',Post::all()); 
+
+    public function index()
+    {
+        return view('posts.index')->with('posts', Post::all());
         //
     }
- 
-  
+
+
     public function create()
 
-    {return view('posts.create')->with('categories',Category::all())->with('tags',Tag::all());
+    {
+        return view('posts.create')->with('categories', Category::all())->with('tags', Tag::all());
         //
     }
 
-  
-    public function store(CreatePostRequest $request){
 
-        $image=$request->image->store('posts');
-        $post= Post::create(['title'=>$request->title,
-        'description'=>$request->description,
-        'content'=>$request->content,
-        'image'=>$image,
-        'published_at'=>$request->published_at,
-        'category_id'=>$request->category,
-        'user_id'=>auth()->user()->id,
-        ]);
-        if($request->tags){
-            $post->tags()->attach($request->tags);
+    public function store(CreatePostRequest $request)
+    {
+        $tags = $request->tags;
+        foreach ($tags as $tag) {
+
+            if (is_numeric($tag)) {
+                $tagArr[] =  $tag;
+            } else {
+                // if the tag not numeric thats meaninig that its new tag and we should create it
+                $newTag = Tag::create(['name' => $tag]);
+
+                // the new Tag id is 3 for exaple
+                $tagArr[] = $newTag->id;
+            }
         }
-        session()->flash('success','Post created successfully');
-       return redirect(route('posts.index'));
-    
+
+
+        $image = $request->image->store('posts');
+        $post = Post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'content' => $request->content,
+            'image' => $image,
+            'published_at' => $request->published_at,
+            'category_id' => $request->category,
+            'user_id' => auth()->user()->id,
+        ]);
+        if ($request->tags) {
+            // $post->tags()->attach($request->tags);
+            $post->tags()->sync($tagArr);
+        }
+        session()->flash('success', 'Post created successfully');
+        return redirect(route('posts.index'));
     }
 
-   
+
     public function edit(Post $post)
     {
-        
-        return view('posts.create')->with('post',$post)->with('categories',Category::all())->with('tags',Tag::all());
+
+        return view('posts.create')->with('post', $post)->with('categories', Category::all())->with('tags', Tag::all());
         //
     }
 
-   
+
     public function update(UpdatePostRequest $request, Post $post)
-    { 
-          $data=$request->only(['title','description','content','published_at']);
+    {
+        $data = $request->only(['title', 'description', 'content', 'published_at']);
 
-          if($request->hasFile('image')){
-          $image= $request->image->store('posts');
-        //   Storage::delete($post->image);
-        $post->delete_image();
-          $data['image']=$image;
-          }
+        if ($request->hasFile('image')) {
+            $image = $request->image->store('posts');
+            //   Storage::delete($post->image);
+            $post->delete_image();
+            $data['image'] = $image;
+        }
 
-          if($request->tags){
-              $post->tags()->sync($request->tags);
-          }
-          $post->update($data);
-          session()->flash('success','Post Updated  Successfully');
-          return redirect(route('posts.index'));
+
+        $tags = $request->tags;
+        foreach ($tags as $tag) {
+
+            if (is_numeric($tag)) {
+                $tagArr[] =  $tag;
+            } else {
+                // if the tag not numeric thats meaninig that its new tag and we should create it
+                $newTag = Tag::create(['name' => $tag]);
+
+                // the new Tag id is 3 for exaple
+                $tagArr[] = $newTag->id;
+            }
+        }
+
+        if ($request->tags) {
+            $post->tags()->sync($tagArr);
+        }
+        $post->update($data);
+        session()->flash('success', 'Post Updated  Successfully');
+        return redirect(route('posts.index'));
 
         //
     }
 
-  
+
     public function destroy($id)
-    {   $post=Post::withTrashed()->where('id',$id)->firstOrFail();
-        
-        if($post->trashed()){
-            // Storage::delete($post->image); used in the Post model due to code refractoring 
+    {
+        $post = Post::withTrashed()->where('id', $id)->firstOrFail();
+
+        if ($post->trashed()) {
+            // Storage::delete($post->image); used in the Post model due to code refractoring
             $post->delete_image();
             $post->forceDelete();
-            session()->flash('success','Post deleted  permanently');
-        } 
-        else{
+            session()->flash('success', 'Post deleted  permanently');
+        } else {
             $post->delete();
-            session()->flash('success','Post trashed successfully');
+            session()->flash('success', 'Post trashed successfully');
         }
-      
+
         return  redirect(route('posts.index'));
         //
     }
 
-    public function treashed(){
-        $trashed=Post::onlyTrashed()->get();
-        return view('posts.index')->with('posts',$trashed);
+    public function treashed()
+    {
+        $trashed = Post::onlyTrashed()->get();
+        return view('posts.index')->with('posts', $trashed);
     }
-    public function restore($id){
-        $post=Post::withTrashed()->where('id',$id)->firstOrFail();
+    public function restore($id)
+    {
+        $post = Post::withTrashed()->where('id', $id)->firstOrFail();
         $post->restore();
-        session()->flash('success','Post Restored successfully');
+        session()->flash('success', 'Post Restored successfully');
         return redirect()->back();
-
     }
-
-
-    
+    public function show(Post $post, Category $category, Tag $tag)
+    {
+        return view('blog.show')->with('post', $post)->with('category', $category)->with('tag', $tag);
+        //
+    }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,107 +1,111 @@
 @extends('layouts.app')
 @section('title')
 
-Create Post    
+    Create Post
 @endsection
 
 @section('content')
 
 
-<div class="card card-default">
-    <div class="card-header">
-    
-      {{isset($post)?'Update post':'Create post'}}  {{--dynamically viewing the  update /create category title--}}
+    <div class="card card-default">
+        <div class="card-header">
 
-    </div>
-    <div class="card-body">
-       
-    @include('partials.error')
-        <form action="{{isset($post) ? route('posts.update',$post->id):route('posts.store')}}" method="post" enctype="multipart/form-data">
-            @csrf
-            @if (isset($post))
-            @method('PUT')
-                
-            @endif
-            <div class="form-group">
-                <label for="Title">Title:</label>
-                <input type="text" class="form-control" id="title" name="title" value=" {{isset($post)? $post->title :''}}">
-            </div>
-            <div class="form-group">
-                <label for="description">Description:</label>
-                <textarea  class="form-control" id="description" name="description" cols="30" rows="5"> {{isset($post)? $post->description :''}}</textarea>
-            </div> 
-            
-            <div class="form-group "  >
-                    
-            <textarea id="my-editor" name="content" class="form-control"> {{isset($post)? $post->content:''}}</textarea>
-             </div> 
-            @if (isset($post))
-            <div class="form-group">
-                <img src="{{asset('/storage/'.$post->image)}}" alt="" height="60px" width="60px">
-            </div>
-                
-            @endif
-            
-            <div class="form-group">
-                <label for="Image">Image:</label>
-                <input type="file" class="form-control-file" id="image" name="image" value=" {{isset($post)? $post->image :''}}">
-            </div>
-            <div class='form-group'>
-                <label for="category">Category</label>
-                <select name="category" id="category" class="form-control">
-                    @foreach ($categories as $category)
-                        <option value="{{$category->id}}"
-                            @if (isset($post))
-                            @if ($category->id=$post->category_id)
-                            selected
-                         @endif
-                                
-                            @endif  >
-                            {{$category->name}}
-                        </option>
-                    @endforeach
-                   
+            {{ isset($post) ? 'Update post' : 'Create post' }} {{-- dynamically viewing the  update /create category title --}}
 
-                </select>
-            </div>
-            @if($tags->count()>0)
+        </div>
+        <div class="card-body">
+
+            @include('partials.error')
+            <form action="{{ isset($post) ? route('posts.update', $post->id) : route('posts.store') }}" method="post"
+                enctype="multipart/form-data">
+                @csrf
+                @if (isset($post))
+                    @method('PUT')
+
+                @endif
                 <div class="form-group">
-                    <label for="post_tag">Tags</label>
-                
-                        <select name="tags[]" id="tags" class="tag_selector form-control" multiple>
-                            @foreach ($tags as $tag)
-                                <option value="{{$tag->id}}"
-                                  @if (isset($post))
-                                        @if ($post->hasTag($tag->id))
-                                        selected  
-                                        @endif
-                                      
-                                  @endif
-                                    >
-                                    
-                                    {{$tag->name}}</option>                            
-                            @endforeach
-                        </select>
-
-                 
+                    <label for="Title">Title:</label>
+                    <input type="text" class="form-control" id="title" name="title"
+                        value=" {{ isset($post) ? $post->title : '' }}">
                 </div>
+                <div class="form-group">
+                    <label for="description">Description:</label>
+                    <textarea class="form-control" id="description" name="description" cols="30"
+                        rows="5"> {{ isset($post) ? $post->description : '' }}</textarea>
+                </div>
+
+                <div class="form-group ">
+
+                    <textarea id="my-editor" name="content"
+                        class="form-control"> {{ isset($post) ? $post->content : '' }}</textarea>
+                </div>
+                @if (isset($post))
+                    <div class="form-group">
+                        <img src="{{ asset('/storage/' . $post->image) }}" alt="" height="60px" width="60px">
+                    </div>
+
                 @endif
 
+                <div class="form-group">
+                    <label for="Image">Image:</label>
+                    <input type="file" class="form-control-file" id="image" name="image"
+                        value=" {{ isset($post) ? $post->image : '' }}">
+                </div>
+                <div class='form-group'>
+                    <label for="category">Category</label>
+                    <select name="category" id="category" class="form-control">
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}" @if (isset($post))
+                                @if ($category->id = $post->category_id)
+                                    selected
+                                @endif
+
+                        @endif >
+                        {{ $category->name }}
+                        </option>
+                        @endforeach
+
+
+                    </select>
+                </div>
+                @if ($tags->count() > 0)
+                    <div class="form-group">
+                        <label for="post_tag">Tags</label>
+
+                        <select name="tags[]" id="tags" class="tag_selector form-control" multiple>
+                            @foreach ($tags as $tag)
+                                <option value="{{ $tag->id }}" @if (isset($post))
+                                    @if ($post->hasTag($tag->id))
+                                        selected
+                                    @endif
+
+                            @endif
+                            >
+
+                            {{ $tag->name }}</option>
+                @endforeach
+                </select>
+
+
+        </div>
+        @endif
 
 
 
-              <div class="form-group">
-                <label for="Published_at">Published At:</label>
-                <input type="text"  id="published_at" name="published_at" value=" {{isset($post)? $post->published_at :''}}">
-            </div>
-        
-            <div class="form-group">
-               <button class="btn btn-success"> {{isset($post)?'Update post':'Create post'}}</button>
-            </div>
+
+        <div class="form-group">
+            <label for="Published_at">Published At:</label>
+            <input type="text" id="published_at" name="published_at"
+                value=" {{ isset($post) ? $post->published_at : '' }}">
+        </div>
+
+        <div class="form-group">
+            <button class="btn btn-success"> {{ isset($post) ? 'Update post' : 'Create post' }}</button>
+        </div>
         </form>
 
     </div>
-</div>
+    </div>
 
 
 
@@ -113,46 +117,50 @@ Create Post
 @section('script')
 
 
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 
 
-<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-<script>
-    // ClassicEditor
-    //         .create( document.querySelector( '#editor' ) )
-    //         .then( editor => {
-    //                 console.log( editor );
-    //         } )
-    //         .catch( error => {
-    //                 console.error( error );
-    //         } );
-    
-            flatpickr('#published_at',{
-                enableTime: true,
-                enableSeconds:true,
-                altFormat: "F j, Y",
-                 dateFormat: "Y-m-d H:i",
-                 defaultDate: "today",
-            defaultHour:new Date().getHours(),
-            defaultMinute:new Date().getMinutes()
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <script>
+        // ClassicEditor
+        //         .create( document.querySelector( '#editor' ) )
+        //         .then( editor => {
+        //                 console.log( editor );
+        //         } )
+        //         .catch( error => {
+        //                 console.error( error );
+        //         } );
+
+        flatpickr('#published_at', {
+            enableTime: true,
+            enableSeconds: true,
+            altFormat: "F j, Y",
+            dateFormat: "Y-m-d H:i",
+            defaultDate: "today",
+            defaultHour: new Date().getHours(),
+            defaultMinute: new Date().getMinutes()
+        });
+
+        $(document).ready(function() {
+            $('.tag_selector').select2({
+                tags: true,
+                tokenSeparators: [',', ' ']
+
             });
-    
-            $(document).ready(function() {
-    $('.tag_selector').select2();
-});
-</script>
+        });
+    </script>
 
-<script src="//cdn.ckeditor.com/4.6.2/standard/ckeditor.js"></script>
-<script>
-  var options = {
-    filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
-    filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
-    filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
-    filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
-  };
-  
-CKEDITOR.replace('my-editor', options);
-</script>
+    <script src="//cdn.ckeditor.com/4.6.2/standard/ckeditor.js"></script>
+    <script>
+        var options = {
+            filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+            filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+            filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+            filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+        };
+
+        CKEDITOR.replace('my-editor', options);
+    </script>
 
 
 
@@ -170,8 +178,6 @@ CKEDITOR.replace('my-editor', options);
 
 @section('css')
 
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 @endsection
-
-
